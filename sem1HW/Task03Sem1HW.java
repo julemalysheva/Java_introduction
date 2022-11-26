7f2d3ebd767d9ca146bdcd473c0ba128badd73c9
julemalysheva <julemalysheva@gmail.com>
2022-11-26 22:25:27 +0500

    Task03*Sem1HW add logger

--- sem1HW/Task03Sem1HW.java
+++ sem1HW/Task03Sem1HW.java
@@ -1,8 +1,13 @@
 package sem1HW;
 
-import java.util.Scanner;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /*Реализовать простой калькулятор */
 
@@ -11,14 +16,16 @@ import java.util.List;
 пока не работает с отрицательными значениями и скобками.
  */
 
+/*По заданию семинара 2 добавлено логирование*/
+
 public class Task03Sem1HW {
 
     // парсим строку и получаем список чисел типа Double
     static List<Double> strToArray(String str) {
-        String[] expArr = str.trim().split("\\s*(\\+|\\*|\\/|-)\\s*");
+        String[] expArr = str.trim().split("\\s*[+*/-]\\s*");
         List<Double> numbers = new ArrayList<>();
-        for (int i = 0; i < expArr.length; i++) {
-            numbers.add(Double.parseDouble(expArr[i]));
+        for (String s : expArr) {
+            numbers.add(Double.parseDouble(s));
         }
 
         return numbers;
@@ -27,8 +34,8 @@ public class Task03Sem1HW {
     // получаем список арифметических операций
     static List<String> strToArrOperation(String str) {
         List<String> arrOperation = new ArrayList<>();
-        String[] znaki = str.split("\\s*(\\d+\\.?\\d*)\\s*");
-        for (String string : znaki) {
+        String[] operation = str.split("\\s*(\\d+\\.?\\d*)\\s*");
+        for (String string : operation) {
             if (!string.isEmpty())
                 arrOperation.add(string);
         }
@@ -38,32 +45,36 @@ public class Task03Sem1HW {
     // производится арифм. операция
     static double operation(String math_operation, Double x, Double y) throws Exception {
         double res = 0;
-        if (math_operation.equals("+"))
-            res = x + y;
-        else if (math_operation.equals("-"))
-            res = x - y;
-        else if (math_operation.equals("/")) {
-            if (y!=0) res = x / y;
-            else throw new Exception("Деление на --0--!");
-        }       
-        else if (math_operation.equals("*"))
-            res = x * y;
+        switch (math_operation) {
+            case "+":
+                res = x + y;
+                break;
+            case "-":
+                res = x - y;
+                break;
+            case "/":
+                if (y != 0) res = x / y;
+                else throw new Exception("Деление на --0--!");
+                break;
+            case "*":
+                res = x * y;
+                break;
+        }
         return res;
     }
 
     // определяем порядок расчета и обращения к спискам с числами и знаками
     static double calc(List<Double> numbers, List<String> arrOperation) throws Exception {
-        int index = 0;
+        int index;
         double tempCalc;
         // проводим расчеты по порядку с *|/
         while ((arrOperation.contains("*")) || (arrOperation.contains("/"))) {
-            if (arrOperation.indexOf("*") == -1)
+            if (!arrOperation.contains("*"))
                 index = arrOperation.indexOf("/");
-            else if (arrOperation.indexOf("/") == -1)
+            else if (!arrOperation.contains("/"))
                 index = arrOperation.indexOf("*");
             else
-                index = (arrOperation.indexOf("*") < arrOperation.indexOf("/")) ? arrOperation.indexOf("*")
-                        : arrOperation.indexOf("/");
+                index = Math.min(arrOperation.indexOf("*"), arrOperation.indexOf("/"));
 
             tempCalc = operation(arrOperation.get(index), numbers.get(index), numbers.get(index + 1));
             // записываем полученный результат на место первого числа в операции
@@ -75,13 +86,12 @@ public class Task03Sem1HW {
 
         // после проводим расчеты с +|-
         while ((arrOperation.contains("+")) || (arrOperation.contains("-"))) {
-            if (arrOperation.indexOf("+") == -1)
+            if (!arrOperation.contains("+"))
                 index = arrOperation.indexOf("-");
-            else if (arrOperation.indexOf("-") == -1)
+            else if (!arrOperation.contains("-"))
                 index = arrOperation.indexOf("+");
             else
-                index = (arrOperation.indexOf("+") < arrOperation.indexOf("-")) ? arrOperation.indexOf("+")
-                        : arrOperation.indexOf("-");
+                index = Math.min(arrOperation.indexOf("+"), arrOperation.indexOf("-"));
 
             tempCalc = operation(arrOperation.get(index), numbers.get(index), numbers.get(index + 1));
             // записываем полученный результат на место первого числа в операции
@@ -94,23 +104,45 @@ public class Task03Sem1HW {
         return numbers.get(0);
     }
 
+    static void myLog(String res, int level) {
+        //level = 1 - ошибки, = 2 - инфо
+        Logger logger = Logger.getLogger(Task03Sem1HW.class.getName());
+        try {
+            FileHandler fh = new FileHandler("sem1HW/logCalc.txt", true);
+            logger.addHandler(fh);
+            SimpleFormatter sFormat = new SimpleFormatter();
+            fh.setFormatter(sFormat);
+            switch (level) {
+                case 1 -> logger.log(Level.WARNING, res);
+                case 2 -> logger.info(res);
+            }
+            fh.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            //throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
         System.out.print("\nВведите строку выражения,\nнапример, 25.5-9/3+7*2.45; 45/9 и т.д.: ");
         String expString = iScanner.nextLine();
-        System.out.printf("Считаем %s\n", expString);
+        myLog(String.format("Пользователь ввел выражение: %s", expString),2);
         iScanner.close();
 
         try {
-            List<Double> numbers = strToArray(expString);
             // вывожу значения для проверки
-            System.out.println(numbers);
+            List<Double> numbers = strToArray(expString);
+            myLog(String.format("Получили числа: %s", numbers),2);
             List<String> arrOperation = strToArrOperation(expString);
-            System.out.println(arrOperation);
+            myLog(String.format("Получили знаки: %s", arrOperation),2);
+
             double resCalc = calc(numbers, arrOperation);
+            myLog(String.format("Результат вычисления = %.2f", resCalc),2);
             System.out.printf("Результат вычисления:\n%s = %.2f\n", expString, resCalc);
 
         } catch (Exception e) {
+            myLog(String.format("Некорректное выражение, ошибка: %s",e.getMessage()),1);
             System.out.println("Некорректное выражение");
             System.err.println(e.getMessage());
         }
