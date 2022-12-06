a168f0f1337fa71faf4e85ef13e447a6994b5f49
julemalysheva <julemalysheva@gmail.com>
2022-12-06 20:18:19 +0500

    Seminar5Task04-арабские в римские

--- sem5/Task04Sem5.java
+++ sem5/Task04Sem5.java
@@ -1,19 +1,19 @@
 package sem5;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 /*Написать метод, который переведёт данное целое число в римский формат.
 Например, 2022 = MMXXII
  1994 -> MCMXCIV
+ 2922; //MMCMXXII
+ 583 - DLXXXIII
 */
 public class Task04Sem5 {
     public static void main(String[] args) {
-        int num = 2922;
+        int num = 583;
         System.out.println(convertToRoman(num));
     }
-    //здесь явно описываю каждый раз, но подумать, быть может использовать treemap, где есть возможность
-    //при опр-х условиях обращаться к ближайшему меньшему или большему - для универсальности решения
+
     static String convertToRoman(int num){
         HashMap<Integer,Character> convert = new HashMap<>();
         convert.put(1, 'I');
@@ -23,9 +23,8 @@ public class Task04Sem5 {
         convert.put(100,'C');
         convert.put(500,'D');
         convert.put(1000,'M');
-        //или в билдере добавлять в начало, к единицам десятки впереди и т.д.?
         StringBuilder sb = new StringBuilder();
-        LinkedHashMap<Integer, Integer> categoriesNum = new LinkedHashMap<>();
+        TreeMap<Integer, Integer> categoriesNum = new TreeMap<>();
         int i = 1;
         while (num!=0){
             categoriesNum.put(i,num%10);
@@ -36,26 +35,34 @@ public class Task04Sem5 {
         //или определить свой порядок хранения элементов?
         //или добавлять в ЛинкедЛист например, вперед
         System.out.println(categoriesNum);
-        //добавляю тысячи
-        for (int j = 0; j < categoriesNum.get(1000); j++) {
-            sb.append(convert.get(1000));
-        }
-        //работаем с сотнями
-        if (categoriesNum.get(100)>=5 && categoriesNum.get(100)<9) {
-            sb.append(convert.get(500));
-            for (int j = 0; j < categoriesNum.get(100)-5; j++) {
-                sb.append(convert.get(100));
+
+        while (!categoriesNum.isEmpty()) {
+            Map.Entry<Integer, Integer> lastEntry = categoriesNum.pollLastEntry();
+            Integer key = lastEntry.getKey();
+            Integer val = lastEntry.getValue();
+//        System.out.println("Removed lastEntry : " + lastEntry );}
+            if (key == 1000){
+                //добавляю тысячи - если значение в цикле, т.е. >0
+                for (int j = 0; j < val; j++) {
+                    sb.append(convert.get(key));
+            }} else {
+            //работаем с сотнями, десятками и единицами
+            if (val >= 5 && val < 9) {
+                sb.append(convert.get(5*key));
+                for (int j = 0; j < val - 5; j++) {
+                    sb.append(convert.get(key));
+                }
+            } else if (val > 0 && val < 4) {
+                for (int j = 0; j < val; j++) {
+                    sb.append(convert.get(key));
+                }
+            } else if (val == 9) {
+                sb.append(convert.get(key)).append(convert.get(10*key));
+            } else if (val == 4) {
+                sb.append(convert.get(key)).append(convert.get(5*key));
+            }
             }
         }
-        else if (categoriesNum.get(100)>0 && categoriesNum.get(100)<4) {
-            for (int j = 0; j < categoriesNum.get(100); j++) {
-                sb.append(convert.get(100));
-            }
-        } else if (categoriesNum.get(100)==9) {
-            sb.append(convert.get(100)).append(convert.get(1000));
-        }
-        //sb.reverse(); //MCMM -не пойдет, разворот посимвольно
-        System.out.println(sb); //или можно развернуть стринг билдер? если это типа массив строк, а не посимвольно
         return sb.toString();
     }
 }
