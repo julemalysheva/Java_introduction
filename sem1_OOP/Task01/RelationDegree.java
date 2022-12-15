a25177b6c1b8407a2d9e30bd9c303ad9da614a46
julemalysheva <julemalysheva@gmail.com>
2022-12-15 16:24:01 +0500

    RelationDegree - created a separate class

---
+++ sem1_OOP/Task01/RelationDegree.java
@@ -0,0 +1,5 @@
+package sem1_OOP.Task01;
+//прочитав https://skr.sh/sHPP49mgsp6 - вынесла в отдельный класс
+public enum RelationDegree{
+    Children, Mother, Father, GrandMother, GrandFather, Sister, Brother, Spouse, GrandChild
+}
